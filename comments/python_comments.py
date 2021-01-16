074d5eef773232af597437b479b90a298c289db0
Balamurugan Thayalan <balamurugan.th@gmail.com>
2021-01-16 13:05:07 +0530

    Python comments - python_comments.py

---
+++ comments/python_comments.py
@@ -0,0 +1,36 @@
+# Python Comments
+
+# Single line comment
+
+print("End of line comment, which will ignore anything after # and treat is as comment")  # This is End of line comment
+
+# This
+#  is
+#    Multi line comments
+print("Multi line comment is nothing but using the # to be prefixed at start of the line.")
+
+"""
+Well this
+is also another way to write
+multi line comments.
+This is actually a string value in multi line
+since this is not assigned to any variable,
+it is treated as multi line comments.
+
+Let's see it by assigning a variable and print the variable
+to see what will happen. 
+"""
+
+print(f"Lets print the multiline string")
+
+
+x = 4
+y = 2
+z = 0
+# z = x + y
+# z = x - y
+print(z)
+print("This is used while debugging, its single line "
+      "comment but we have commented the code logic itself"
+      " which will be ignored by the Python interpreter while executing")
+
