b222114c203a74c464d5ef495d72c455fcc42044
Balamurugan Thayalan <balamurugan.th@gmail.com>
2021-01-16 13:06:33 +0530

    Assignment operators in Python - assignment_operators.py

---
+++ operators/assignment_operators.py
@@ -0,0 +1,25 @@
+"""
+Python Assignment Operators
+Assignment operators are used to assign values to variables:
+---------------------------------------------------------
+|   Operator        |   Example     |       Similar to  |
+|-------------------|---------------|-------------------|
+|       =           |   x = 5       |       x = 5       |
+|       +=          |   x += 5      |       x = x + 5   |
+|       -=          |   x -= 5      |       x = x - 5   |
+|       *=          |   x *= 5      |       x = x * 5   |
+|       /=          |   x /= 5      |       x = x / 5   |
+|       %=          |   x %= 5      |       x = x % 5   |
+|       //=         |   x //= 5     |       x = x // 5  |
+|       **=         |   x **= 5     |       x = x ** 5  |
+|       &=          |   x &= 5      |       x = x & 5   |   Bitwise AND
+|       |=          |   x |= 5      |       x = x | 5   |   Bitwise OR
+|       ^=          |   x ^= 5      |       x = x ^ 5   |   Bitwise XOR
+|       >>=         |   x >>= 5     |       x = x >> 5  |   Bitwise right shift
+|       <<=         |   x <<= 5     |       x = x << 5  |   Bitwise left shift
+----------------------------------------------------------
+"""
+x = 10
+x = ~4
+print(~x)
+
