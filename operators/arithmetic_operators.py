57c1cabf160f3777ebebcc236b3b8b7d7e1f4c8a
Balamurugan Thayalan <balamurugan.th@gmail.com>
2021-01-16 13:06:14 +0530

    Arithmetic operators in Python - arithmetic_operators.py

---
+++ operators/arithmetic_operators.py
@@ -0,0 +1,30 @@
+"""
+Python Arithmetic Operators
+Arithmetic operators are used with numeric values to perform common mathematical operations:
+---------------------------------------------------------
+|   Operator    |   Name        |       Example         |
+|---------------|---------------|-----------------------|
+|       +       |   Addition    |       x + y           |
+|       -       |   Subtraction |       x - y           |
+|       *       | Multiplication|       x * y           |
+|       /       |   Division    |       x / y           |
+|       %       |   Modulus     |       x % y           |
+|       **      |Exponentiation |       x ** y          |
+|       //      | Floor division|       x // y          |
+--------------------------------------------------------
+"""
+
+x = 2
+y = 4
+print(f"x               = {x}")
+print(f"y               = {y}")
+print(f"Addition        x + y = {x + y}")
+print(f"Subtraction     x - y = {x - y}")
+print(f"Multiplication  x * y = {x * y}")
+print(f"Division        x / y = {x / y}")
+print(f"Modulus         x % y = {x % y}")
+print(f"Exponentiation  x ** y = {x ** y}")
+x = 20.90
+y = 2.8
+print(f"Floor division  x // y = {x // y}")
+print(f"Division        x / y = {x / y}")
