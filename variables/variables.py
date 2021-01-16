0633cb6fca91a27b43712e9da112347f672e78af
Balamurugan Thayalan <balamurugan.th@gmail.com>
2021-01-16 14:02:30 +0530

    Variables in Python - variables.py

---
+++ variables/variables.py
@@ -0,0 +1,21 @@
+#  Variables
+
+age = 20                    # declaring int variable
+temperature = 89.8          # declaring float variable
+name = 'John'               # declaring str variable, Note: we use single quotes to store the text.
+model = "SD902"             # declaring str variable
+print(model)
+
+model = 8890                # now re-declaring model variable as int
+
+# In Python this is allowed, anywhere you can change the type of variable
+# just be re declaring with new value of any type.
+print(model)
+
+# Another way of declaring variables
+# also the variable names are case sensitive
+
+msg = str("Big Brother is in town")
+Msg = str("Case sensitive variable")
+print(f"msg = {msg}")
+print(f"Msg = {Msg}")
